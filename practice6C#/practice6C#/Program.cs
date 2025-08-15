6ffe1b7827c4224ad76a860c1d7dbf1b4ce95c7a
Mariya Krechmer <sulroman2006@mail.ru>
2025-08-15 15:38:40 +0700

    Начал новый практический файл, испробовал первый виртуальный метод

---
+++ practice6C#/practice6C#/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice6C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NonPlayerCharacter[] characters =
+            {
+                new NonPlayerCharacter(),
+                new Farmer(),
+                new Knight(),
+                new Child()
+            };
+
+            foreach (var character in characters)
+            {
+                character.ShowDescription();
+                Console.WriteLine(new string('-',40));
+            }
+        }
+    }
+    
+    class NonPlayerCharacter
+    {
+        public virtual void ShowDescription()
+        {
+            Console.WriteLine("Я простой NPC, умею только гулять");
+        }
+    }
+
+    class Farmer : NonPlayerCharacter
+    {
+        public override void ShowDescription()
+        {
+            base.ShowDescription();
+            Console.WriteLine("А еще я фермер и умею работать на поле.");
+        }
+    }
+
+    class Knight : NonPlayerCharacter
+    {
+        public override void ShowDescription()
+        {
+            Console.WriteLine("Я рыцарь, мое дело - это сражения");
+        }
+    }
+
+    class Child : NonPlayerCharacter
+    {
+
+    }
+}
---
+++ practice6C#/practice6C#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("practice6C#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("practice6C#")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0317a6d6-2d2c-4248-b46e-e11653cf6530")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
