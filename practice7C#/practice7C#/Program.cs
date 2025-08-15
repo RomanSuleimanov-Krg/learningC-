ece6fd3b5e34822d1328b04df93cd84393c8de01
Mariya Krechmer <sulroman2006@mail.ru>
2025-08-16 01:44:05 +0700

    начал новое практическое задание

---
+++ practice7C#/practice7C#/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice7C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Beehaviour
+    {
+        public virtual void Update()
+        {
+
+        }
+    }
+
+    class Walker : Beehaviour
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Иду");
+        }
+    }
+
+    class Jumper : Walker
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Прыгаю");
+        }
+    }
+}
