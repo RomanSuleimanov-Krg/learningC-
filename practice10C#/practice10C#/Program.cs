fd52242ead4cbb526e13627dd73b13a91cc0ded7
RomanSuleimanov <sulroman2006@mail.ru>
2025-09-03 21:24:44 +0700

    Изучил абстрактные классы. Начал изучать интерфейсы, попробовал сделать реализацию интерфейса классом.

---
+++ practice10C#/practice10C#/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice10C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataProcessor dataProcessor = new ConsoleDataProcessor();
+            dataProcessor.ProcessData(new DbDataProvider());
+            dataProcessor.ProcessData(new FileDataProvider());
+            dataProcessor.ProcessData(new APIDataProvider());
+        }
+    }
+
+    interface IDataProvider
+    {
+        string GetData();
+    }
+
+    interface IDataProcessor
+    {
+        void ProcessData(IDataProvider dataProvider);
+    }
+
+    class ConsoleDataProcessor : IDataProcessor
+    {
+        public void ProcessData(IDataProvider dataProvider)
+        {
+            Console.WriteLine(dataProvider.GetData());
+        }
+    }
+
+    class DbDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Вот тут были бы данные из БД";
+        }
+    }
+
+    class FileDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Вот тут были бы данные из файла";
+        }
+    }
+
+    class APIDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Вот тут были бы данные из API";
+        }
+    }
+}
---
+++ practice9C#/practice9C#/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice9C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            Weapon[] inventory = {new Gun(), new LaserGun(), new Bow()};
+
+            foreach(var item in inventory)
+            {
+                player.CheckInfo(item);
+                player.Fire(item);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    abstract class Weapon
+    {
+        public abstract void Fire();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(GetType().Name);
+        }
+    }
+
+    class Gun : Weapon
+    {
+        public override void Fire()
+        {
+            Console.WriteLine("Пыщ!");
+        }
+    }
+
+    class LaserGun : Weapon
+    {
+        public override void Fire()
+        {
+            Console.WriteLine("Пиу!");
+        }
+    }
+
+    class Bow : Weapon
+    {
+        public override void Fire()
+        {
+            Console.WriteLine("Пау!");
+        }
+    }
+
+    class Player
+    {
+        
+        public void CheckInfo(Weapon weapon)
+        {
+            weapon.ShowInfo();
+        }
+
+
+        public void Fire(Weapon weapon)
+        {
+            weapon.Fire();
+        }
+    }
+}
