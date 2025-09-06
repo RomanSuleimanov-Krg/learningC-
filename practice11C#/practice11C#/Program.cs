6a66fa9c86d3ea6e5167340d64c781420d46ba3d
RomanSuleimanov <sulroman2006@mail.ru>
2025-09-06 12:27:03 +0700

    Абстрактный класс + применение

---
+++ practice11C#/practice11C#/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice11C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = new Car();
+
+            car.Move();
+        }
+    }
+
+    abstract class Vehicle
+    {
+        protected float Speed;
+
+        public abstract void Move();
+
+        public float GetCurrentSpeed()
+        {
+            return Speed;
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Звук ревущего мотора!!!");
+        }
+    }
+}
